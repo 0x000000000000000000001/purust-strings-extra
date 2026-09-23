b1e77f582d6868d6f343302cf10297d387f8a7ec
0x1 <kevin.francart3@gmail.com>
2026-09-23 18:48:15 +0200

    misc

--- src/Data/String/Extra.rs
+++ src/Data/String/Extra.rs
@@ -1,7 +1,4 @@
-pub fn Data_String_Extra_levenshtein(mut a_val: crate::UnknownType, mut b_val: crate::UnknownType) -> crate::UnknownType {
-    let a = a_val.unwrap_string();
-    let b = b_val.unwrap_string();
-    
+pub fn Data_String_Extra_levenshtein(a: String, b: String) -> i64 {
     let a_chars: Vec<char> = a.chars().collect();
     let b_chars: Vec<char> = b.chars().collect();
     
@@ -24,23 +21,19 @@ pub fn Data_String_Extra_levenshtein(mut a_val: crate::UnknownType, mut b_val: c
         }
     }
     
-    let dist = d[a_chars.len()][b_chars.len()] as i64;
-    crate::Value::Int(dist)
+    d[a_chars.len()][b_chars.len()] as i64
 }
 
-pub fn Data_String_Extra_sorensenDiceCoefficient(mut a_val: crate::UnknownType, mut b_val: crate::UnknownType) -> crate::UnknownType {
-    let a = a_val.unwrap_string();
-    let b = b_val.unwrap_string();
-    
+pub fn Data_String_Extra_sorensenDiceCoefficient(a: String, b: String) -> f64 {
     if a == b {
-        return crate::Value::Number(1.0);
+        return 1.0;
     }
     
     let a_chars: Vec<char> = a.chars().collect();
     let b_chars: Vec<char> = b.chars().collect();
     
     if a_chars.len() < 2 || b_chars.len() < 2 {
-        return crate::Value::Number(0.0);
+        return 0.0;
     }
     
     let mut a_bigrams = std::collections::HashMap::new();
@@ -60,7 +53,5 @@ pub fn Data_String_Extra_sorensenDiceCoefficient(mut a_val: crate::UnknownType, 
         }
     }
     
-    let coeff = (2.0 * intersection as f64) / ((a_chars.len() - 1 + b_chars.len() - 1) as f64);
-    
-    crate::Value::Number(coeff)
+    (2.0 * intersection as f64) / ((a_chars.len() - 1 + b_chars.len() - 1) as f64)
 }
